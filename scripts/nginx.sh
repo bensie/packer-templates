3d5ef6fdd4600536c6e45b970e86bea31e4d28a0
James Miller <bensie@gmail.com>
2014-08-20 17:10:41 -0700

    Don't do too much cloud-init, fix nginx issue

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
 
 # Add HTTPS support for APT
-apt-get install apt-transport-https ca-certificates
+apt-get install -y apt-transport-https ca-certificates
 
 # Create /etc/apt/sources.list.d/passenger.list
 echo 'deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main' > /etc/apt/sources.list.d/passenger.list
